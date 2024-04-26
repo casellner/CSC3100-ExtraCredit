a3a7472bd04adda5814caf9c3e9b25e1885fbac6
casellner <114782256+casellner@users.noreply.github.com>
2024-04-25 22:33:05 -0500

    finished tutorial to make server work

--- node-server/index.js
+++ node-server/index.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var fs = require('fs');
+const { Server } = require("socket.io")
 
 var index = fs.readFileSync('index.html');
 
@@ -10,13 +11,21 @@ const port = new SerialPort({ path: 'COM5', baudRate: 9600 })
 
 const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }))
 
-parser.on('data', function(data){
-    console.log(data);
-});
-
-var app = http.createServer(function(req,res){
+var app = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type':'text/html'});
     res.end(index);
 })
+
+//var io = require('socket.io');
+const io = new Server(app)
+
+io.httpServer.on('connection', function(data){
+    console.log('Node.js is listening!')
+})
+
+parser.on('data', function(data){
+    console.log(data);
+    io.emit('data', data);
+});
 
 app.listen(3000);
