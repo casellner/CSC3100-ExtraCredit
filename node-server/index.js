1305fa83532ee16ccbb65e555669d4bb8f4d6654
casellner <114782256+casellner@users.noreply.github.com>
2024-04-25 20:57:32 -0500

    create index.html

--- node-server/index.js
+++ node-server/index.js
@@ -1,3 +1,8 @@
+var http = require('http');
+var fs = require('fs');
+
+var index = fs.readFileSync('index.html');
+
 const { SerialPort } = require('serialport')
 
 const { ReadlineParser } = require('@serialport/parser-readline')
@@ -8,3 +13,10 @@ const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }))
 parser.on('data', function(data){
     console.log(data);
 });
+
+var app = http.createServer(function(req,res){
+    res.writeHead(200, {'Content-Type':'text/html'});
+    res.end(index);
+})
+
+app.listen(3000);
