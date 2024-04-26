f50a79435c41774ff7472758d764743aa84477aa
casellner <114782256+casellner@users.noreply.github.com>
2024-04-25 23:09:27 -0500

    Update Arduino code

--- arduino-code/version1.ino
+++ arduino-code/version1.ino
@@ -2,13 +2,27 @@
 
 */
 
-int variable = 777;
+#include <DHT.h>
+#define DHTTYPE DHT11
+#define DHTPIN  4
+
+DHT dht(DHTPIN, DHTTYPE, 11); // 11 works fine for ESP8266
+
+//int variable = 777;
+float humidity, temp_f;  // Values read from sensor
 
 void setup() {
   Serial.begin(9600);
+  dht.begin();           // initialize temperature sensor
 }
 
 void loop() {
-  Serial.println(variable);
+  //Serial.println(variable);
+
+  humidity = dht.readHumidity();          // Read humidity (percent)
+  temp_f = dht.readTemperature(true);     // Read temperature as Fahrenheit
+
+  Serial.println(humidity);
+  
   delay(100);
 }
